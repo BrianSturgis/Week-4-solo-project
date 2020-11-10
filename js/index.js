13356dc7501900f44c52acbbccbe608e5b755834
Brian Sturgis <sturujisu@gmail.com>
2020-11-09 19:56:11 -0800

    add the pizzACost object

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 function Order(pie, sauce, firstTopping, size) {
-this.pie
+  this.pie = pie
+  this.sauce = sauce
+  this.firstTopping = firstTopping
+  this.size = size
+
 }
+
+Let pizzaCost = {
+  pie: 
+  sauce:
+  firstTopping:
+  size:
+  price: 
+
+};
+
+$(document).ready(function(){
+  $("#inputNumber").submit(function(event){
+    event.preventDefault();
+
+  });
+});
