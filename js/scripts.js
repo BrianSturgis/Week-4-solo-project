5d5f55eba8c0afe6d8a9a75b6d1e49a08791d630
Brian Sturgis <sturujisu@gmail.com>
2020-11-11 19:35:51 -0800

    add the Order constructor to scripts.js pre testing

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,12 @@
 //  calculates produces output
 
 function Order(pie, sauce, firstTopping, size, updatePrice) {
+  this.orderDetails = [];
   this.pie = pie;
   this.sauce = sauce;
   this.firstTopping = firstTopping;
   this.size = size;
   this.updatePrice = updatePrice;
-  price = function() {
-    return ++this.updatePrice;
-  };
 }
 
 let Order1 = Order();
