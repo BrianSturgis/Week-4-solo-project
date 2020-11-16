727cfc156bc0f20bd1f4e050a48491a499203abd
Brian Sturgis <sturujisu@gmail.com>
2020-11-15 21:04:33 -0800

    the final commit before re sub

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,23 @@
 //  calculates produces output
 
 function Order(pie, sauce, firstTopping, size) {
-  // console.log(this);
   this.pie = pie;
   this.sauce = sauce;
   this.firstTopping = firstTopping;
-  // console.log(firstTopping);
   this.size = size;
 }
 
+Order.prototype.finalCost = function(){
+  
+};
 
-$(document).ready(function(){
+
+$(document).ready(function(){ 
   $("form#questions").submit(function(event){
     event.preventDefault();
     let pie = parseInt($("#question1").val());
-    // console.log(pie);
     let sauce = parseInt($("#question2").val());
     let firstTopping = parseInt($("#question3").val());
-    // console.log(firstTopping);
     let size = parseInt($("#question4").val());
     let cost = new Order(pie, sauce, firstTopping, size);
     console.log(cost);
